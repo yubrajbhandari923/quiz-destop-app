1923d8982763f414779cb48e2ec208061dacadf8
yubrajbhandari923 <yubrajbhandari32@yahoo.com>
2019-10-04 14:26:31 +0545

    made changes and almost completed index

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ function createWindow () {
   })
 
   // and load the index.html of the app.
-  win.loadFile('index.html')
+  win.loadFile('src/html/index.html')
 
   // Open the DevTools.
 //   win.webContents.openDevTools()
