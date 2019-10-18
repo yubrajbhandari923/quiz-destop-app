9cb547d9ff3a413f8fdf5f130a5a1ca61022a2c6
yubrajbhandari923 <yubrajbhandari32@yahoo.com>
2019-10-18 12:28:56 +0545

    nothing

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let userdata = {
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win
-
+// l;ets  go
 function createWindow () {
   // Create the browser window.
   win = new BrowserWindow({
